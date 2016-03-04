6b8c8bf6b946b1dceec6f2c096604179f08fe9b6
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-03-04 13:44:30 +0100

    Added dummy code

--- server/main.js
+++ server/main.js
@@ -2,11 +2,19 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello world');
+    res.send('Hello world');
 });
 
-/* Use PORT environment variable if it exists */
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.send('index');
+});
+
+app.use('/api/hour', router );
+
+
 var port = process.env.PORT || 5000;
 server = app.listen(port, function () {
-  console.log('Server listening on port %d in %s mode', server.address().port, app.settings.env);
+    console.log('Server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
